d76da188d60a76e5ea33fec1839ba9245b1d9a90
ben_ten_11 <bjornhephaestus@proton.me>
2026-04-19 23:11:51 -0700

    just fucking around, learning some of the libs Im going to use to complete this project

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,43 @@
 #include <algorithm>
-#include <chrono>
 #include <cmath>
 #include <math.h>
+#include <matplot/matplot.h>
 #include <ncpp/NotCurses.hh>
 #include <ncpp/Plane.hh>
 #include <ncpp/Visual.hh>
 #include <notcurses/notcurses.h>
 #include <stdint.h>
 #include <thread>
+
+/**
+ * @brief Generates a sine wave graph and saves it as a GIF.
+ * * @param filename The output file name (must end in .gif)
+ */
+void generate_gif_graph(const std::string &filename) {
+  using namespace matplot;
+
+  // 1. Generate sample data (a smooth sine wave)
+  std::vector<double> x = linspace(0, 2 * pi, 100);
+  std::vector<double> y = transform(x, [](double val) { return std::sin(val); });
+
+  // 2. Create the figure in "quiet mode" (true) to prevent UI pop-ups
+  auto fig = figure(true);
+
+  // 3. Plot the data and customize the visual appearance
+  auto p = plot(x, y);
+  p->line_width(2.5);
+  p->color("blue");
+
+  // Add labels and title
+  title("Sine Wave Plot");
+  xlabel("X-Axis (Radians)");
+  ylabel("Amplitude");
+
+  // 4. Save the figure
+  // The library sees the ".gif" extension and automatically uses the GIF
+  // terminal
+  save(filename);
+}
 
 uint32_t get_fire_to_indigo(double t) {
   // Clamp t between 0 and 1 to prevent weird colors
@@ -29,21 +59,18 @@ uint32_t get_fire_to_indigo(double t) {
 }
 
 int main() {
+  generate_gif_graph("test_gif.png");
   ncpp::NotCurses nc;
   ncpp::Plane *stdp = nc.get_stdplane();
   ncpp::Visual ncv("assets/reaper.jpg");
 
+  struct ncvisual_options vopts = {.n = nullptr,
+                                   .scaling = NCSCALE_SCALE,
+                                   .y = 2,
+                                   .x = 5,
+                                   .blitter = NCBLIT_BRAILLE};
 
-  struct ncvisual_options vopts = {
-    .n = nullptr,
-    .scaling = NCSCALE_SCALE,
-    .y = 2,
-    .x = 5,
-    .blitter = NCBLIT_BRAILLE
-  };
-
-
-  ncpp::Plane* img_plane = ncpp::Plane::from_ncplane(ncv.blit(&vopts));
+  // ncpp::Plane* img_plane = ncpp::Plane::from_ncplane(ncv.blit(&vopts));
   nc.render();
 
   ncinput ni;
@@ -56,13 +83,11 @@ int main() {
     double t = 0.5 + 0.5 * std::sin(time);
     uint32_t curr_color = get_fire_to_indigo(t);
     stdp->set_fg_rgb(curr_color);
-    stdp->putstr(10,10, "Welcome to to-be-done");
+    stdp->putstr(10, 10, "Welcome to to-be-done");
     nc.render();
-
 
     time += 0.05;
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
-
   }
 
   return 0;
